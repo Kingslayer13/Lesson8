e6fbda87f5d4a75d0bcb1dcc37b12fe7c735206f
Kingslayer13 <3700511l>
2013-11-25 20:44:58 +0200

    создание елементов через жКвыри

--- main8.js
+++ main8.js
@@ -2,11 +2,9 @@
  * Created by оо on 25.11.13.
  */
 ;(function($){
-    var input = $("input");
-    console.log(input.prop("value", "sergiy"));
-    console.log(input.prop({value: "asd", title: "amam"})); //like object
-    console.log(input.prop("title", function(index, value){
-        return "artem";
-    }));
+   $("<input type='password'>").prop(
+       {value: "asd"}
+   ).appendTo("body")
+
 
 })(jQuery);
